813cd3ddaa8f8eff7cea196e672a2d488fa01cfc
harsha-n <sriharsha.nistala@outlook.com>
2024-07-04 10:10:05 -0700

    Content away from Navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,74 +1,90 @@
 // components/NavBar.tsx
-import { HStack, Icon, Box } from '@chakra-ui/react'
-import { FaHome, FaUser, FaFolder, FaEnvelope } from 'react-icons/fa'
+import { HStack, Icon, Box, Flex } from "@chakra-ui/react";
+import { FaHome, FaUser, FaFolder } from "react-icons/fa";
 import { BsStack } from "react-icons/bs";
-import { motion } from 'framer-motion'
-import Link from 'next/link'
-import { useRouter } from 'next/navigation';
+import { motion } from "framer-motion";
+import { useRouter } from "next/navigation";
 
-const MotionBox = motion(Box)
+const MotionBox = motion(Box);
 
-const NavBar = ({ activeSection , setActiveSection }: { activeSection: string; setActiveSection: Function } ) => {
+const NavBar = ({
+  activeSection,
+  setActiveSection,
+}: {
+  activeSection: string;
+  setActiveSection: Function;
+}) => {
   const router = useRouter();
   const navItems = [
-    { icon: FaHome, section: '/' },
-    { icon: FaUser, section: 'about' },
-    { icon: FaFolder, section: 'projects' },
-    { icon: BsStack  , section: 'experiences' },
+    { icon: FaHome, section: "/" },
+    { icon: FaUser, section: "about" },
+    { icon: FaFolder, section: "projects" },
+    { icon: BsStack, section: "experiences" },
     // { icon: FaEnvelope, section: 'contact' },
-  ]
+  ];
 
   return (
-    <MotionBox
-      initial={{ opacity: 0, y: 20 }}
-      animate={{ opacity: 1, y: 0 }}
-      transition={{ duration: 0.5 }}
-      position="fixed"
-      bottom="20px"
-    //   left="50%"
-      transform="translateX(-50%)"
-      zIndex={1000}
-    >
-      <HStack spacing={10} bg="blue.800" p={4} borderRadius="full" boxShadow="lg">
-        {navItems.map((item, index) => (
-          <Box
-            key={index}
-            as="button"
-            onClick={() => {setActiveSection(item.section); router.push(item.section)}}
-            position="relative"
-          >
-            <MotionBox
-              whileHover={{ scale: 1.2 }}
-              whileTap={{ scale: 0.9 }}
-              transition={{ type: 'spring', stiffness: 300 }}
+    <Flex justifyContent="center">
+      <MotionBox
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        position="fixed"
+        bottom="20px"
+        //   left="50%"
+        transform="translateX(-50%)"
+        zIndex={1000}
+      >
+        <HStack
+          spacing={10}
+          bg="blue.800"
+          p={4}
+          borderRadius="full"
+          boxShadow="lg"
+        >
+          {navItems.map((item, index) => (
+            <Box
+              key={index}
+              as="button"
+              onClick={() => {
+                setActiveSection(item.section);
+                router.push(item.section);
+              }}
+              position="relative"
             >
-              <Icon
-                as={item.icon}
-                boxSize={6}
-                color={activeSection === item.section ? 'white' : 'gray.300'}
-              />
-            </MotionBox>
-            {activeSection === item.section && (
-              <motion.div
-                layoutId="activeIndicator"
-                style={{
-                  position: 'absolute',
-                  bottom: '-10px',
-                  left: '50%',
-                  width: '5px',
-                  height: '5px',
-                  borderRadius: '50%',
-                  backgroundColor: '#4789e1',
-                  // transform: 'translateX(-50%)',
-                }}
-                transition={{ type: 'spring', stiffness: 300, damping: 30 }}
-              />
-            )}
-          </Box>
-        ))}
-      </HStack>
-    </MotionBox>
-  )
-}
+              <MotionBox
+                whileHover={{ scale: 1.2 }}
+                whileTap={{ scale: 0.9 }}
+                transition={{ type: "spring", stiffness: 300 }}
+              >
+                <Icon
+                  as={item.icon}
+                  boxSize={6}
+                  color={activeSection === item.section ? "white" : "gray.300"}
+                />
+              </MotionBox>
+              {activeSection === item.section && (
+                <motion.div
+                  layoutId="activeIndicator"
+                  style={{
+                    position: "absolute",
+                    bottom: "-10px",
+                    left: "50%",
+                    width: "5px",
+                    height: "5px",
+                    borderRadius: "50%",
+                    backgroundColor: "#4789e1",
+                    // transform: 'translateX(-50%)',
+                  }}
+                  transition={{ type: "spring", stiffness: 300, damping: 30 }}
+                />
+              )}
+            </Box>
+          ))}
+        </HStack>
+      </MotionBox>
+    </Flex>
+  );
+};
 
-export default NavBar
+export default NavBar;
--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,31 +1,35 @@
-import Profile from "../components/Profile";
-import SocialIcons from "../components/SocialIcons";
 import { motion, AnimatePresence } from "framer-motion";
-import { Box, VStack, Heading, Text, Button, HStack } from "@chakra-ui/react";
+import {
+  Box,
+  Flex,
+  Center,
+} from "@chakra-ui/react";
 import dynamic from "next/dynamic";
 import { ReactElement, useState } from "react";
 import React from "react";
+
 const DynamicNavBar = dynamic(() => import("../components/NavBar"), {
   ssr: false,
 });
+
 const MotionBox = motion(Box);
+
 export default function Layout({ children }: { children: ReactElement }) {
   const [activeSection, setActiveSection] = useState("home");
 
   return (
-    <>
+    <Flex direction="column" minH="100vh" bg="navy.900" color="white">
       <MotionBox
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
         transition={{ duration: 0.5 }}
-        bg="navy.900"
-        minH="100vh"
-        color="white"
+        flex="1"
         display="flex"
         flexDirection="column"
         alignItems="center"
         justifyContent="center"
         px={4}
+        pb={16}
       >
         <AnimatePresence mode="wait">
           <MotionBox
@@ -38,11 +42,15 @@ export default function Layout({ children }: { children: ReactElement }) {
             <main>{children}</main>
           </MotionBox>
         </AnimatePresence>
-        <DynamicNavBar
-          activeSection={activeSection}
-          setActiveSection={setActiveSection}
-        />
       </MotionBox>
-    </>
+      <Center position="fixed" bottom={0} left={0} right={0}>
+        <Box maxWidth="container.md" width="100%">
+          <DynamicNavBar
+            activeSection={activeSection}
+            setActiveSection={setActiveSection}
+          />
+        </Box>
+      </Center>
+    </Flex>
   );
 }
